388dffa18648e1b99e9e76087c7b275e3d1a6d22
Hyunwoo Kim <khyw407@gmail.com>
2018-12-20 12:29:54 +0900

    add consul configuration

--- src/main/java/com/test/springboot/AccountServiceApplication.java
+++ src/main/java/com/test/springboot/AccountServiceApplication.java
@@ -2,8 +2,8 @@ package com.test.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,7 +11,7 @@ import com.test.springboot.domain.Account;
 import com.test.springboot.repository.AccountRepository;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class AccountServiceApplication {
 	
 	@LoadBalanced
